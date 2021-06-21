7672c7ba97e9dc87cff9f226fb7ab4e488f042e4
Dzmitry Harhun <dimagorga@gmail.com>
2021-06-21 14:46:07 +0300

    добавляет разметку и рандомное изменение цветов боди

---
+++ main.js
@@ -0,0 +1,46 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('button[data-action="start"]'),
+    stop: document.querySelector('button[data-action="stop"]'),
+    isActive: false,
+} 
+let swithColorID = null;   
+refs.start.addEventListener('click', onStartButton)
+refs.stop.addEventListener('click', onStopButton)
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };  
+
+function onStartButton() {
+    if(refs.isActive){
+        return
+    }
+    refs.isActive = true;
+    refs.start.setAttribute('disabled', 'disabled')
+    swithColorID = setInterval(() => {   
+        refs.body.style.backgroundColor = colors[randomColorMaker()]
+    }, 1000)
+}
+function onStopButton(){ 
+    refs.isActive = false
+    clearInterval(swithColorID)
+    refs.body.style.backgroundColor = '#ffffff'
+    refs.start.removeAttribute('disabled', 'disabled')
+}
+function randomColorMaker(){
+    return randomIntegerFromInterval(0, colors.length-1)
+}
+
+
+
+  
